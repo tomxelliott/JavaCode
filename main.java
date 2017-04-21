1e39c956c6b107edb481287e26488f9620e4ac14
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-04-21 12:47:29 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 public class main {
 
-public static void main(String [] args) {
-  NameGenerator ng = 
-}
+  public static void main(String [] args) {
+    NameGenerator ng = new NameGenerator("Tom", "Elliott", "Goldman", "Ashford");
+    ng.generateStarWarsName();
+  }
 }
