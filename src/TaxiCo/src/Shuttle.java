e596f622bffc2900ffaa69d33e439ad1a2b544f5
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-05-27 13:49:17 +0100

    Create Shuttle.java

---
+++ src/TaxiCo/src/Shuttle.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+/**
+ * A shuttle.
+ * Shuttles have a list of destinations stored on their route.
+ * They operate a circular route.
+ * 
+ * @author Tom Elliott 
+ * @version 2016.12.03
+ */
+public class Shuttle extends Vehicle
+{
+    // The circular route of this shuttle.
+    private ArrayList<String> route;
+    // The destination number in route that the shuttle is currently headed for.
+    private int destinationNumber;
+
+    /**
+     * Constructor for objects of class Shuttle
+     * @param id This shuttle's unique id.
+     * @param route The route taken by this shuttle.
+     *              The first entry is the starting location.
+     */
+    public Shuttle(String id, ArrayList<String> route)
+    {
+        super(id);
+        setRoute(route);
+    }
+    
+    /**
+     * Indicate that this shuttle has arrived at its next destination.
+     */
+    public void arrived()
+    {
+        setLocation(getDestination());
+        setNextDestination();
+    }
+    
+    /**
+     * Set the next destination of the shuttle.
+     */
+    private void setNextDestination()
+    {
+        destinationNumber++;
+        if(destinationNumber >= route.size()) {
+            // End of the circular route.
+            // Start from the beginning again.
+            destinationNumber = 0;
+        }
+        setDestination(route.get(destinationNumber));
+    }
+    
+    /**
+     * Set the route for this shuttle.
+     * @param route The circular list of destinations.
+     */
+    private void setRoute(ArrayList<String> route)
+    {
+        // Make a copy of the list parameter.
+        this.route = new ArrayList<String>();
+        this.route.addAll(route);
+        destinationNumber = 0;
+        setLocation(route.get(destinationNumber));
+        setNextDestination();
+    }
+}
