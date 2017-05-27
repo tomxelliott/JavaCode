3d09b035fca4b0a2b8b2f8245fbf21087c7e6ae6
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-05-27 13:51:53 +0100

    Create CommandWord.java

---
+++ src/DrawTool/src/CommandWord.java
@@ -0,0 +1,12 @@
+ /**
+ * This class is specifically for storing all of the help commands.
+ * Can easily add the help commands here which will in turn be printed out when the user type the "help" command.
+ * 
+ * @author (Tom Elliott) 
+ * @version (1.0 - 23rd Nov 2016)
+ */
+public enum CommandWord
+{
+ // Listed below are the command words that the user can use in the  DrawingTool draw() method.
+    PEN, SELECT, DELETE, UP, DOWN, MOVE, MOVETO, TURN, TURNTO, COLOUR, HELP, BYE
+}
