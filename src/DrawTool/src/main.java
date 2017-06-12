9ab79bfef00b36d5455b7b6482e1626b689c2c8c
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-06-12 11:26:14 +0100

    Create main.java

---
+++ src/DrawTool/src/main.java
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * @author (Tom Elliott) 
+ * @version (1.0 - 29/11/16)
+ * 
+ */
+public class main
+{
+    /**
+     * main method for DrawingTool
+     */
+    public static void main(String [] args)
+    {
+        DrawingTool dt = new DrawingTool();
+        dt.draw();
+    }
+}
