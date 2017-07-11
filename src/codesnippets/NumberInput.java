bf71a3a6b1d159e03b180e63dbe58fe7606e8d68
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-07-11 23:02:56 +0100

    Update and rename numberInput.java to NumberInput.java

--- src/codesnippets/NumberInput.java
+++ src/codesnippets/NumberInput.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class NumberInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
