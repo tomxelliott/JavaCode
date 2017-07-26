4c74dfc66a1c8ffdc7e978e0c601d306b4243f28
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-07-26 20:52:21 +0100

    Create Colours.java

---
+++ src/codesnippets/Lights/Colours.java
@@ -0,0 +1,12 @@
+/**
+ * I created this class to not only keep the LightColours class for getting cluttered,
+ * but also thought it was a nice clean way to store the colours that can be used in the program.
+ * Colours can easily be added or removed here and there is no tight coupling between the classes that use the colours.
+ *
+ * @author (Tom Elliott)
+ * @ 1.0 (24/07/17)
+ */
+public enum Colours {
+    // Listed below are the colours that will be used in the program.
+    RED, GREEN, WHITE, BLUE, YELLOW, PURPLE, ORANGE, PINK, INDIGO, VIOLET
+}
