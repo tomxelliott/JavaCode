5712403e56ea8836f2c62c97caab3df3cb4b8f10
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-07-26 20:42:23 +0100

    Update SquareRootSolution.java

--- src/codesnippets/SquareRootSolution.java
+++ src/codesnippets/SquareRootSolution.java
@@ -1,11 +1,18 @@
-
-public void squares(int a, int b) {
-        String result = "";
-        for(int c = a; c <= b; c++) {
-            long squareRoot = (int) Math.sqrt(c);
-            if(squareRoot*squareRoot == c) {
-                result = result + c + " ";
-            }
+class SquareRootSolution {
+        
+        public SquareRootSolution(){
+                squares(1,10);
+                squares(10,100);
         }
-        System.out.println(result);
-    }
+        
+        public void squares(int a, int b) {
+                String result = "";
+                for(int c = a; c <= b; c++) {
+                long squareRoot = (int) Math.sqrt(c);
+                if(squareRoot*squareRoot == c) {
+                        result = result + c + " ";
+                }
+                }
+                System.out.println(result);
+        }
+}
