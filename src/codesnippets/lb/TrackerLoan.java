be69d8d9bd90a55f945546586c3ceb80c23eb23b
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-08-14 17:03:01 +0100

    Create TrackerLoan.java

---
+++ src/codesnippets/lb/TrackerLoan.java
@@ -0,0 +1,15 @@
+/**
+ * This subclass contains any features or functionality specific to a Tracker Loan.
+ *
+ * At this moment in time, the Tracker Loans and Fixed Loans do not have any features
+ * that distinguish themselves from one another however that will likely change in the
+ * future and this can easily be implemented using this inheritance structure.
+ *
+ * Created by tomelliott on 30/07/2017.
+ */
+public class TrackerLoan extends Loans {
+
+    public TrackerLoan(int loanID, int loanAmount, String product, int loanTerm, String completedDate) {
+        super(loanID, loanAmount, product, loanTerm, completedDate);
+    }
+}
